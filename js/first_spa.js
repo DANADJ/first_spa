9c42e0a7db2c862da55ef8ea0a60e28b7eb9da07
Valentin Gordienko <danadj@yandex.ru>
2017-07-19 23:59:27 +0300

    DEV: URI controls is created

--- js/first_spa.js
+++ js/first_spa.js
@@ -79,6 +79,12 @@
 		firstSpaShellChatId: '#first-spa-shell-chat'
 	},
 	    configMap = {
+		anchorSchemaMap: {
+			chat: {
+				open: true,
+				closed: true
+			}
+		},
 		mainHtml: '<div class="first-spa-shell-head">\n\t\t\t\t\t\t<div class="first-spa-shell-head-logo"></div>\n\t\t\t\t\t\t<div class="first-spa-shell-head-acct"></div>\n\t\t\t\t\t\t<div class="first-spa-shell-head-search"></div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class="first-spa-shell-main">\n\t\t\t\t\t\t<div class="first-spa-shell-main-nav"></div>\n\t\t\t\t\t\t<div class="first-spa-shell-main-content"></div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class="first-spa-shell-foot"></div>\n\t\t\t\t\t<div class="first-spa-shell-chat" id="' + Ids.firstSpaShellChatId.substr(1) + '"></div>\n\t\t\t\t\t<div class="first-spa-shell-modal"></div>',
 		chatExtendTime: 1000,
 		chatRetractTime: 300,
@@ -89,14 +95,97 @@
 	},
 	    stateMap = {
 		$container: null,
+		anchorMap: {},
 		isChatRetracted: true
 	},
 	    jqueryMap = {};
 
 	//----------------- НАЧАЛО СЛУЖЕБНЫХ МЕТОДОВ -------------------
+	function copyAnchorMap() {
+		return $.extend(true, {}, stateMap.anchor_map);
+	}
 	//------------------ КОНЕЦ СЛУЖЕБНЫХ МЕТОДОВ -------------------
 
 	//-------------------- НАЧАЛО МЕТОДОВ DOM ----------------------
+
+	function changeAnchorPart(argMap) {
+		var anchorMapRevise = copyAnchorMap(),
+		    boolReturn = true,
+		    keyName = void 0,
+		    keyNameDep = void 0;
+
+		KEYVAL: for (keyName in argMap) {
+			if (argMap.hasOwnProperty(keyName)) {
+				// пропустить зависимые ключи
+				if (keyName.indexOf('_') === 0) {
+					continue KEYVAL;
+				}
+
+				// обновить значение независимого ключа
+				anchorMapRevise[keyName] = argMap[keyName];
+
+				// обновить соответствующий зависимый ключ
+				keyNameDep = '_' + keyName;
+
+				if (argMap[keyNameDep]) {
+					anchorMapRevise[keyNameDep] = argMap[keyNameDep];
+				} else {
+					delete anchorMapRevise[keyNameDep];
+					delete anchorMapRevise['_s' + keyNameDep];
+				}
+			}
+		}
+		// Конец объединения изменений в хэше якорей
+
+		// Начало попытки обновления URI;
+		// в случае ошибки восстановить исходное состояние
+		try {
+			$.uriAnchor.setAnchor(anchorMapRevise);
+		} catch (error) {
+			// восстановить исходное состояние в URI
+			$.uriAnchor.setAnchor(stateMap.anchorMap, null, true);
+			boolReturn = false;
+		}
+		// Конец попытки обновления URI...
+
+		return boolReturn;
+	}
+
+	function onHashchange(event) {
+		var anchorMapPrevious = copyAnchorMap(),
+		    anchorMapProposed = null,
+		    s_chatProposed = null;
+
+		// пытаемся разобрать якорь
+		try {
+			anchorMapProposed = $.uriAnchor.makeAnchorMap();
+		} catch (error) {
+			$.uriAnchor.setAnchor(anchorMapPrevious, null, true);
+			return false;
+		}
+		stateMap.anchorMap = anchorMapProposed;
+
+		// Начало изменения компонента Chat
+		if (!anchorMapPrevious || anchorMapPrevious._s_chat !== anchorMapProposed._s_chat) {
+			s_chatProposed = anchorMapProposed.chat;
+			switch (s_chatProposed) {
+				case 'open':
+					toggleChat(true);
+					break;
+				case 'closed':
+					toggleChat(false);
+					break;
+				default:
+					toggleChat(false);
+					delete anchorMapProposed.chat;
+					$.uriAnchor.setAnchor(anchorMapProposed, null, true);
+			}
+		}
+
+		// Конец изменения компонента Chat
+		return false;
+	}
+
 	function setJqueryMap() {
 		var $container = stateMap.$container;
 
@@ -106,6 +195,13 @@
 		};
 	}
 
+	/**
+	 * @description - The function opens or closes the chat window
+	 * @function toggleChat
+	 * @param {boolean} doExtend - Parametr specifies to open or close the chat window
+	 * @param {function} [callback] - Function called when the chat window is open or closed
+	 * @returns {boolean} If the chat window is sliding - false, If the chat window is open or closed - true
+	 */
 	function toggleChat(doExtend, callback) {
 		var chatHeight = jqueryMap.$chat.height(),
 		    chatOpen = chatHeight === configMap.chatExtendHeight,
@@ -134,10 +230,13 @@
 	//--------------------- КОНЕЦ МЕТОДОВ DOM ----------------------
 
 	//---------------- НАЧАЛО ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
-	function onClickChat(event) {
-		toggleChat(stateMap.isChatRetracted);
+	function onClickChat() {
+		changeAnchorPart({
+			chat: stateMap.isChatRetracted ? 'open' : 'closed'
+		});
+
 		return false;
-	};
+	}
 	//----------------- КОНЕЦ ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
 
 	//------------------- НАЧАЛО ОТКРЫТЫХ МЕТОДОВ ------------------
@@ -149,6 +248,12 @@
 
 		stateMap.isChatRetracted = true;
 		jqueryMap.$chat.attr('title', configMap.chatRetractedTitle).click(onClickChat);
+
+		$.uriAnchor.configModule({
+			schema_map: configMap.anchorSchemaMap
+		});
+
+		$(window).bind('hashchange', onHashchange).trigger('hashchange');
 	}
 	//------------------- КОНЕЦ ОТКРЫТЫХ МЕТОДОВ -------------------
 
--- source/js/first_spa.shell.js
+++ source/js/first_spa.shell.js
@@ -2,6 +2,12 @@ let Ids = {
 		firstSpaShellChatId: '#first-spa-shell-chat',
 	},
 	configMap = {
+		anchorSchemaMap: {
+			chat: {
+				open: true,
+				closed: true
+			}
+		},
 		mainHtml: `<div class="first-spa-shell-head">
 						<div class="first-spa-shell-head-logo"></div>
 						<div class="first-spa-shell-head-acct"></div>
@@ -23,15 +29,100 @@ let Ids = {
 	},
 	stateMap = {
 		$container: null,
-		isChatRetracted: true
+		anchorMap: {},
+		isChatRetracted: true,
 	},
 	jqueryMap = {};
 
 
 //----------------- НАЧАЛО СЛУЖЕБНЫХ МЕТОДОВ -------------------
+function copyAnchorMap() {
+	return $.extend(true, {}, stateMap.anchor_map);
+}
 //------------------ КОНЕЦ СЛУЖЕБНЫХ МЕТОДОВ -------------------
 
 //-------------------- НАЧАЛО МЕТОДОВ DOM ----------------------
+
+function changeAnchorPart(argMap) {
+	let anchorMapRevise = copyAnchorMap(),
+		boolReturn = true,
+		keyName,
+		keyNameDep;
+
+	KEYVAL:
+		for (keyName in argMap) {
+			if (argMap.hasOwnProperty(keyName)) {// пропустить зависимые ключи
+				if (keyName.indexOf('_') === 0) {
+					continue KEYVAL;
+				}
+
+				// обновить значение независимого ключа
+				anchorMapRevise[keyName] = argMap[keyName];
+
+				// обновить соответствующий зависимый ключ
+				keyNameDep = '_' + keyName;
+
+				if (argMap[keyNameDep]) {
+					anchorMapRevise[keyNameDep] = argMap[keyNameDep];
+				} else {
+					delete anchorMapRevise[keyNameDep];
+					delete anchorMapRevise['_s' + keyNameDep];
+				}
+			}
+		}
+	// Конец объединения изменений в хэше якорей
+
+	// Начало попытки обновления URI;
+	// в случае ошибки восстановить исходное состояние
+	try {
+		$.uriAnchor.setAnchor(anchorMapRevise);
+	} catch (error) {
+		// восстановить исходное состояние в URI
+		$.uriAnchor.setAnchor(stateMap.anchorMap, null, true);
+		boolReturn = false;
+	}
+	// Конец попытки обновления URI...
+
+	return boolReturn;
+}
+
+function onHashchange(event) {
+	let anchorMapPrevious = copyAnchorMap(),
+		anchorMapProposed = null,
+		s_chatProposed = null;
+
+	// пытаемся разобрать якорь
+	try {
+		anchorMapProposed = $.uriAnchor.makeAnchorMap();
+	} catch (error) {
+		$.uriAnchor.setAnchor(anchorMapPrevious, null, true);
+		return false;
+	}
+	stateMap.anchorMap = anchorMapProposed;
+
+
+	// Начало изменения компонента Chat
+	if (!anchorMapPrevious || anchorMapPrevious._s_chat !== anchorMapProposed._s_chat) {
+		s_chatProposed = anchorMapProposed.chat;
+		switch (s_chatProposed) {
+			case 'open' :
+				toggleChat(true);
+				break;
+			case 'closed' :
+				toggleChat(false);
+				break;
+			default :
+				toggleChat(false);
+				delete anchorMapProposed.chat;
+				$.uriAnchor.setAnchor(anchorMapProposed, null, true);
+		}
+	}
+
+	// Конец изменения компонента Chat
+	return false;
+}
+
+
 function setJqueryMap() {
 	let $container = stateMap.$container;
 
@@ -41,6 +132,13 @@ function setJqueryMap() {
 	};
 }
 
+/**
+ * @description - The function opens or closes the chat window
+ * @function toggleChat
+ * @param {boolean} doExtend - Parametr specifies to open or close the chat window
+ * @param {function} [callback] - Function called when the chat window is open or closed
+ * @returns {boolean} If the chat window is sliding - false, If the chat window is open or closed - true
+ */
 function toggleChat(doExtend, callback) {
 	let chatHeight = jqueryMap.$chat.height(),
 		chatOpen = chatHeight === configMap.chatExtendHeight,
@@ -77,10 +175,13 @@ function toggleChat(doExtend, callback) {
 //--------------------- КОНЕЦ МЕТОДОВ DOM ----------------------
 
 //---------------- НАЧАЛО ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
-function onClickChat(event) {
-	toggleChat(stateMap.isChatRetracted);
+function onClickChat() {
+	changeAnchorPart({
+		chat: ( stateMap.isChatRetracted ? 'open' : 'closed' )
+	});
+
 	return false;
-};
+}
 //----------------- КОНЕЦ ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
 
 //------------------- НАЧАЛО ОТКРЫТЫХ МЕТОДОВ ------------------
@@ -94,6 +195,12 @@ function initModule($container) {
 	jqueryMap.$chat
 		.attr('title', configMap.chatRetractedTitle)
 		.click(onClickChat);
+
+	$.uriAnchor.configModule({
+		schema_map: configMap.anchorSchemaMap
+	});
+
+	$(window).bind('hashchange', onHashchange).trigger('hashchange');
 }
 //------------------- КОНЕЦ ОТКРЫТЫХ МЕТОДОВ -------------------
 
